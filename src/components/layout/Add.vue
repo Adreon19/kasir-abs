3b0b9635f5cc101b7d737184a8408444689f6cfe
Adreon <sachioramzy12@gmail.com>
2025-01-09 22:09:49 +0700

    feat: add all other page layouts

---
+++ src/components/layout/Add.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Ini buat tambah menu</h1>
+    <p>Nanti bikin component baru, buat bikin card menu</p>
+  </div>
+</template>
---
+++ src/components/layout/History.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Ini History</h1>
+    <p>Nanti bikin component baru, buat bikin card menu</p>
+  </div>
+</template>
