3632d2e48fc932289952db598e430eea8ac6b10b
Adreon <sachioramzy12@gmail.com>
2025-01-09 22:07:39 +0700

    chore: changes of the sidebar location, delete footbar

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,55 +1,54 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <header class="bg-white text-black">
-      <div class="sidebar">
-        <div class="logo flex">
-          <img src="" alt="ABS Logo">
-          <strong style="width: 800px;">Aritisan Beverage Studio</strong>
-        </div>
-        <ul class="m-5">
-          <li><RouterLink :to="{name: ''}"><i class="fa-solid fa-house"></i>Home Page</RouterLink></li>
-          <li><RouterLink :to="{name: ''}"><i class="fa-solid fa-scroll"></i>History Menu</RouterLink></li>
-          <li><RouterLink :to="{name: ''}"><i class="fa-solid fa-cart-shopping"></i>Order</RouterLink></li>
-          <li><RouterLink :to="{name: ''}"><i class="fa-solid fa-square-plus"></i>Add Menu</RouterLink></li>
-        </ul>
+  <div class="flex flex-row min-h-screen">
+    <aside class="sidebar bg-white text-black">
+      <div class="logo flex items-center p-4">
+        <img src="" alt="ABS Logo" class="w-8 h-8 mr-4" />
+        <strong>Aritisan Beverage Studio</strong>
       </div>
-    </header>
+      <ul class="m-5">
+        <RouterLink to="/"
+          ><li><i class="fa-solid fa-house"></i> Home Page</li></RouterLink
+        >
+        <RouterLink to="/history"
+          ><li><i class="fa-solid fa-scroll"></i> History Menu</li></RouterLink
+        >
+        <RouterLink to="/order">
+          <li><i class="fa-solid fa-cart-shopping"></i> Order</li></RouterLink
+        >
+        <RouterLink to="/add"
+          ><li><i class="fa-solid fa-square-plus"></i> Add Menu</li></RouterLink
+        >
+      </ul>
+    </aside>
 
-    <main class="flex-grow">
-      <slot />
-    </main>
-
-    <footer class="bg-gray-800 text-white text-center p-4">
-      <p>&copy; Nanti rapihin footer nya ya atau mau dihapus juga gapapa</p>
-    </footer>
+    <div class="flex-grow flex flex-col">
+      <main class="flex-grow">
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
 
 <style scoped>
-header {
-  position: fixed;
-  top: 0;
-  right: 10px;
+.sidebar {
   width: 350px;
-  height: 100%;
+  height: 100vh;
+  position: sticky;
+  top: 0;
 }
 
-.sidebar li{
+.sidebar li {
   padding: 10px;
   font-size: 20px;
 }
 
-.sidebar li:hover{
+.sidebar li:hover {
   color: #fff;
   background: #000;
-  width: 100%;
+  width: calc(100% - 10px);
 }
 
-.sidebar i{
+.sidebar i {
   margin-right: 15px;
-}
-
-footer {
-  background-color: #ffffff1b;
 }
 </style>
