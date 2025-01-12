355a3a2646a9053886bbf7ec2d95d393f7833146
Adreon <sachioramzy12@gmail.com>
2025-01-12 11:53:33 +0700

    edit: login path layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
+import { useRoute } from "vue-router";
 import Layout from "./components/layout/Layout.vue";
+
+const route = useRoute();
 </script>
+
 <template>
-  <Layout>
-    <router-view />
-  </Layout>
+  <div>
+    <Layout v-if="!['/login', '/register'].includes(route.path)">
+      <router-view />
+    </Layout>
+    <router-view v-else />
+  </div>
 </template>
+
 <style scoped></style>
