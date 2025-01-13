2e05e42be47967320c54baa8b8ae520ed389eed9
Adreon <sachioramzy12@gmail.com>
2025-01-13 16:37:11 +0700

    chore: login direct route page logic

--- src/views/router.js
+++ src/views/router.js
@@ -4,37 +4,56 @@ import Add from "../pages/Add.vue";
 import History from "../pages/History.vue";
 import Order from "../pages/Order.vue";
 import Login from "../pages/login.vue";
+import { supabase } from "../supabase"; // Import your Supabase instance
+
 const routes = [
   {
     path: "/",
     name: "Home",
     component: HomePage,
+    meta: { requiresAuth: true, layout: true }, // Requires authentication
   },
   {
     path: "/add",
     name: "Add",
     component: Add,
+    meta: { requiresAuth: true, layout: true },
   },
   {
     path: "/history",
     name: "History",
     component: History,
+    meta: { requiresAuth: true, layout: true },
   },
   {
     path: "/order",
     name: "Order",
     component: Order,
+    meta: { requiresAuth: true, layout: true },
   },
   {
     path: "/login",
     name: "login",
     component: Login,
+    meta: { requiresAuth: false, layout: false },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (to.meta.requiresAuth && !session) {
+    return next({ name: "login" });
+  }
+
+  next();
 });
 
 export default router;
