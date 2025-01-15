9546911635cd50e13a5a00b1c37e953eb78b8fdc
Adreon <sachioramzy12@gmail.com>
2025-01-15 11:19:04 +0700

    chore: selecting categories logic

--- src/components/items/Header.vue
+++ src/components/items/Header.vue
@@ -1,24 +1,48 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { supabase } from "../../supabase";
 
-const selectedCategories = ref();
-const categories = ref([
-    { name: 'KOPI', code: 'NY' },
-    { name: 'TEH', code: 'RM' },
-    { name: 'MOCKTAIL', code: 'LDN' },
-    { name: 'COCKTAIL', code: 'IST' },
-    { name: 'VODKA', code: 'PRS' }
-]);
+const categories = ref([]);
+const selectedCategories = ref([]);
+
+const fetchCategories = async () => {
+  try {
+    const { data, error } = await supabase
+      .from("kategori_menu")
+      .select("kategori");
+
+    if (error) {
+      throw error;
+    }
+
+    categories.value = data;
+    console.log("Fetched categories:", categories.value);
+  } catch (error) {
+    console.error("Error fetching categories:", error.message);
+  }
+};
+onMounted(fetchCategories);
 </script>
 
-
 <template>
-    <div class="searchBar m-5" style="display: flex; justify-content: space-between;">
-        <h1 style="font-size: 30px;">Home Page</h1>
-        <div class="flex gap-2">
-            <InputText style="width: 250px;" placeholder="Search..." class="p-3 "/><Button icon="fas fa-search" iconPos="top" class="" />   
-        </div>
-        <Select v-model="selectedCategories" :options="categories" optionLabel="name" placeholder="Select Categories" checkmark :highlightOnSelect="false" class="w-full md:w-56" />
+  <div
+    class="searchBar m-5"
+    style="display: flex; justify-content: space-between"
+  >
+    <h1 style="font-size: 30px">Home Page</h1>
+    <div class="flex gap-2">
+      <InputText
+        style="width: 250px"
+        placeholder="Search..."
+        class="p-3"
+      /><Button icon="fas fa-search" iconPos="top" class="" />
     </div>
+    <Select
+      v-model="selectedCategories"
+      :options="categories"
+      optionLabel="kategori"
+      placeholder="Select Categories"
+      class="w-full md:w-56"
+    />
+  </div>
 </template>
-
