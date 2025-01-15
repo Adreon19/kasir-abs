baed3545bd3c5b1846403d00c2316ebdbe87d7b4
Adreon <sachioramzy12@gmail.com>
2025-01-15 11:19:56 +0700

    chore: showing each menu name and image from menu_list table

--- src/components/items/Card.vue
+++ src/components/items/Card.vue
@@ -1,98 +1,69 @@
 <script setup>
-import Card from "primevue/card";
+import { Card } from "primevue";
+import { ref, onMounted } from "vue";
+import { supabase } from "../../supabase";
+const categories = ref([]);
+const menuList = ref([]);
+
+const fetchCategories = async () => {
+  try {
+    const { data, error } = await supabase
+      .from("kategori_menu")
+      .select("kategori");
+
+    if (error) {
+      throw error;
+    }
+
+    categories.value = data;
+    console.log("Fetched categories:", categories.value);
+  } catch (error) {
+    console.error("Error fetching categories:", error.message);
+  }
+};
+
+const fetchMenuList = async () => {
+  try {
+    const { data, error } = await supabase.from("menu_list").select("*");
+    if (error) throw error;
+    menuList.value = data;
+  } catch (err) {
+    console.error("Error fetching menu list:", err.message);
+  }
+};
+
+const initializeData = async () => {
+  try {
+    await fetchCategories();
+    await fetchMenuList();
+  } catch (error) {
+    console.error("Error during initialization:", error.message);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(initializeData);
 </script>
 
 <template>
   <section class="grid grid-cols-4 gap-4 m-4">
-    <Card style="width: 18rem; overflow: hidden">
+    <Card
+      v-for="menu in menuList"
+      :key="menu.id"
+      style="width: 18rem; overflow: hidden"
+    >
       <template #header>
         <img
-          alt="user header"
-          src="https://primefaces.org/cdn/primevue/images/usercard.png"
+          :alt="menu.name"
+          :src="menu.image"
+          class="object-cover h-48 w-full"
         />
       </template>
-      <template #title>Advanced Card</template>
-      <template #subtitle>Card subtitle</template>
-      <template #content>
-        <p class="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-          sed consequuntur error repudiandae numquam deserunt quisquam repellat
-          libero asperiores earum nam nobis, culpa ratione quam perferendis
-          esse, cupiditate neque quas!
-        </p>
-      </template>
+      <template #title>{{ menu.name }}</template>
       <template #footer>
         <div class="flex gap-4 mt-1">
-          <Button label="pesan" class="w-full" />
-        </div>
-      </template>
-    </Card>
-    <Card style="width: 18rem; overflow: hidden">
-      <template #header>
-        <img
-          alt="user header"
-          src="https://primefaces.org/cdn/primevue/images/usercard.png"
-        />
-      </template>
-      <template #title>Advanced Card</template>
-      <template #subtitle>Card subtitle</template>
-      <template #content>
-        <p class="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-          sed consequuntur error repudiandae numquam deserunt quisquam repellat
-          libero asperiores earum nam nobis, culpa ratione quam perferendis
-          esse, cupiditate neque quas!
-        </p>
-      </template>
-      <template #footer>
-        <div class="flex gap-4 mt-1">
-          <Button label="pesan" class="w-full" />
-        </div>
-      </template>
-    </Card>
-    <Card style="width: 18rem; overflow: hidden">
-      <template #header>
-        <img
-          alt="user header"
-          src="https://primefaces.org/cdn/primevue/images/usercard.png"
-        />
-      </template>
-      <template #title>Advanced Card</template>
-      <template #subtitle>Card subtitle</template>
-      <template #content>
-        <p class="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-          sed consequuntur error repudiandae numquam deserunt quisquam repellat
-          libero asperiores earum nam nobis, culpa ratione quam perferendis
-          esse, cupiditate neque quas!
-        </p>
-      </template>
-      <template #footer>
-        <div class="flex gap-4 mt-1">
-          <Button label="pesan" class="w-full" />
-        </div>
-      </template>
-    </Card>
-    <Card style="width: 18rem; overflow: hidden">
-      <template #header>
-        <img
-          alt="user header"
-          src="https://primefaces.org/cdn/primevue/images/usercard.png"
-        />
-      </template>
-      <template #title>Advanced Card</template>
-      <template #subtitle>Card subtitle</template>
-      <template #content>
-        <p class="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-          sed consequuntur error repudiandae numquam deserunt quisquam repellat
-          libero asperiores earum nam nobis, culpa ratione quam perferendis
-          esse, cupiditate neque quas!
-        </p>
-      </template>
-      <template #footer>
-        <div class="flex gap-4 mt-1">
-          <Button label="pesan" class="w-full" />
+          <Button label="Pesan" class="w-full" />
         </div>
       </template>
     </Card>
