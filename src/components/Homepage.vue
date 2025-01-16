7a9200846036c28775a7f6b70110edeebc673443
Adreon <sachioramzy12@gmail.com>
2025-01-16 13:00:54 +0700

    ref: menu details logic

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,11 +1,106 @@
 <script setup>
 import Card from "./items/Card.vue";
-import Header from "./items/Header.vue";
+import { ref, watch, onMounted } from "vue";
+import { supabase } from "../supabase";
+
+const menuList = ref([]);
+const categories = ref([]);
+const selectedCategory = ref(null);
+const description = ref();
+const isLoading = ref(true);
+
+const fetchCategories = async () => {
+  try {
+    const { data, error } = await supabase
+      .from("kategori_menu")
+      .select("id, kategori");
+
+    if (error) {
+      throw error;
+    }
+
+    categories.value = data;
+  } catch (error) {
+    console.error("Error fetching categories:", error.message);
+  }
+};
+
+watch(selectedCategory, async (newCategory) => {
+  menuList.value = await fetchMenuList(newCategory);
+  console.log(newCategory);
+});
+
+const fetchMenuList = async (categoryId = null) => {
+  try {
+    let query = supabase.from("menu_list").select(`
+          name,
+          image,
+          description,
+          kategori_id,
+          menu_detail (
+            price,
+            variant_id,
+            menu_variants(name)
+          )
+      `);
+
+    if (categoryId) {
+      query = query.eq("kategori_id", categoryId);
+    }
+
+    const { data, error } = await query;
+
+    if (error) throw error;
+
+    return data;
+  } catch (err) {
+    console.error("Error fetching menu list:", err.message);
+  }
+};
+
+const initializeData = async () => {
+  try {
+    menuList.value = await fetchMenuList();
+    await fetchCategories();
+  } catch (error) {
+    console.error("Error during initialization:", error.message);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(initializeData);
 </script>
 
 <template>
   <div>
-    <Header />
-    <Card />
+    <div
+      class="searchBar m-5"
+      style="display: flex; justify-content: space-between"
+    >
+      <h1 style="font-size: 30px" class="text-white font-bold">Menu</h1>
+      <div class="search flex relative">
+        <InputText
+          style="width: 400px; border-radius: 10px 0 0 10px"
+          placeholder="Search..."
+          class="search p-3 bg-white text-black font-bold rounded-none focus:outline-none"
+        />
+        <Button
+          style="border-radius: 0 10px 10px 0"
+          icon="fas fa-search"
+          iconPos="top"
+          class="button-search hover:border-none text-black bg-white rounded-none"
+        />
+      </div>
+      <Select
+        v-model="selectedCategory"
+        :options="categories"
+        option-value="id"
+        option-label="kategori"
+        placeholder="Select Categories"
+        class="p-select w-full md:w-56 font-bold"
+      />
+    </div>
+    <Card :menu-list="menuList" />
   </div>
 </template>
--- src/components/items/Card.vue
+++ src/components/items/Card.vue
@@ -1,89 +1,53 @@
 <script setup>
 import { Card } from "primevue";
-import { ref, onMounted } from "vue";
-import { supabase } from "../../supabase";
 import { formatCurrency } from "../../utils/formatter/currency";
 
-const menuList = ref([]);
-
-const fetchMenuList = async () => {
-  try {
-    const { data, error } = await supabase.from("menu_detail").select(`
-        price,
-        hot_price,
-        cold_price,
-        menu_id (
-          name,
-          image
-        )
-      `);
-
-    if (error) throw error;
-
-    menuList.value = data.map((menu) => ({
-      price: menu.price || null,
-      hotWater: menu.hot_price || false,
-      coldWater: menu.cold_price || false,
-      name: menu.menu_id?.name || "Unnamed",
-      image: menu.menu_id?.image || "placeholder.jpg",
-    }));
-  } catch (err) {
-    console.error("Error fetching menu list:", err.message);
-  }
-};
-
-const initializeData = async () => {
-  try {
-    await fetchMenuList();
-  } catch (error) {
-    console.error("Error during initialization:", error.message);
-  }
-};
-
-onMounted(initializeData);
+const { menuList } = defineProps(["menuList"]);
 </script>
 
 <template>
-  <section class="grid grid-cols-4 m-4">
-    <Card
-      v-for="menu in menuList"
-      :key="menu.name"
-      class="menu flex flex-col h-full p-0 rounded-lg"
-    >
-      <template #header>
-        <img
-          :alt="menu.name"
-          :src="menu.image"
-          class="img-menu object-cover h-48 w-full"
-        />
-      </template>
+  <section class="flex flex-col m-4">
+    <div class="grid grid-cols-4 gap-4">
+      <Card
+        v-for="menu in menuList"
+        :key="menu.name"
+        class="menu flex flex-col h-full p-0 rounded-lg"
+      >
+        <template #header>
+          <img
+            :alt="menu.name"
+            :src="menu.image"
+            class="img-menu object-cover h-48 w-full"
+          />
+        </template>
 
-      <template #title>
-        <h3 class="text-xl capitalize font-bold">{{ menu.name }}</h3>
-      </template>
+        <template #title>
+          <h3 class="text-xl capitalize font-bold">{{ menu.name }}</h3>
+        </template>
+        <template #content class="flex-1">
+          <template v-for="detail in menu.menu_detail">
+            <h4 class="mb-2 text-sm text-slate-400">
+              {{ detail.menu_variants.name }}:
+              {{ formatCurrency(detail.price) }}
+            </h4>
+          </template>
 
-      <template #content class="flex-1">
-        <h4 v-if="menu.price !== null" class="mb-2 text-sm text-slate-400">
-          Price: {{ formatCurrency(menu.price) }}
-        </h4>
-        <div class="text-sm text-slate-400 mt-2">
-          <p v-if="menu.hotWater">✔ Hot Water Available</p>
-          <p v-else>✘ Hot Water Not Available</p>
-          <p v-if="menu.coldWater">✔ Cold Water Available</p>
-          <p v-else>✘ Cold Water Not Available</p>
-        </div>
-      </template>
+          <div class="text-sm text-slate-400 mt-2">
+            <p class="text-white">{{ menu.description }}</p>
+          </div>
+        </template>
 
-      <template #footer>
-        <div class="flex gap-4 mt-auto">
-          <Button label="Pesan" class="button w-full" />
-        </div>
-      </template>
-    </Card>
+        <template #footer>
+          <div class="flex gap-4 mt-auto">
+            <Button label="Pesan" class="button w-full" />
+          </div>
+        </template>
+      </Card>
+    </div>
   </section>
 </template>
 
-<style>
+<style scoped>
 .menu {
   background: var(--bg-card);
   padding: 0;
