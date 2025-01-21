a355acc0c19299fcac7cae278737778d1bbf426d
Adreon <sachioramzy12@gmail.com>
2025-01-21 22:39:12 +0700

    chore: fetch order histories

--- src/pages/History.vue
+++ src/pages/History.vue
@@ -1,31 +1,67 @@
 <script setup>
-// import { ref, onMounted } from "vue";
-// import { ProductService } from "@/service/ProductService";
+import { ref, onMounted } from "vue";
+import { supabase } from "../supabase";
+import { formatCurrency } from "../utils/formatter/currency";
 
-// onMounted(() => {
-//   ProductService.getProductsMini().then((data) => (products.value = data));
-// });
+const order = ref([]);
 
-// const products = ref();
-// const columns = [
-//   { field: "code", header: "Code" },
-//   { field: "name", header: "Name" },
-//   { field: "category", header: "Category" },
-//   { field: "quantity", header: "Quantity" },
-// ];
+const fetchOrder = async () => {
+  try {
+    let { data: order_detail, error } = await supabase.from("order_detail")
+      .select(`
+      id, 
+      order_id(
+        id,
+        customer_name,
+        payment(
+          id,
+          name
+        ),
+        paid,
+        total_price
+      ), 
+      menu_detail_id(
+        menu_id(
+          id,
+          name
+        )
+      ),
+      quantity,
+      note
+      `);
+    if (error) throw error;
+    order.value = order_detail;
+  } catch (error) {
+    console.log("error fetching order:", error);
+  }
+};
+
+onMounted(fetchOrder);
 </script>
 
 <template>
-  <div class="m-5">
-    <h1 style="font-size: 30px">History Menu</h1>
-  </div>
+  <div class="p-6 flex flex-col gap-6">
+    <h1 style="font-size: 30px">Riwayat Pesanan</h1>
+    <section class="main-section">
+      <DataTable :value="order" stripedRows tableStyle="min-width: 50rem">
+        <Column field="order_id.customer_name" header="Customer Name" />
+        <Column field="order_id.payment.name" header="Payment Method" />
+        <Column field="menu_detail_id.menu_id.name" header="Menu Name" />
+        <Column field="quantity" header="Quantity" />
+        <Column field="note" header="Note" />
+        <Column header="Total">
+          <template #body="slotProps">
+            {{ formatCurrency(slotProps.data.order_id.total_price) }}
+          </template>
+        </Column>
 
-  <div class="">
-    <DataTable :value="products" tableStyle="min-width: 50rem">
-      <Column field="code" header="Code"></Column>
-      <Column field="name" header="Name"></Column>
-      <Column field="category" header="Category"></Column>
-      <Column field="quantity" header="Quantity"></Column>
-    </DataTable>
+        <!-- Custom rendering for Paid column -->
+        <Column header="Paid">
+          <template #body="slotProps">
+            {{ formatCurrency(slotProps.data.order_id.paid) }}
+          </template>
+        </Column>
+      </DataTable>
+    </section>
   </div>
 </template>
