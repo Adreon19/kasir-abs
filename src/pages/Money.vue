bb0a96571135d3d83387379eef6c51744a4209b3
Adreon <sachioramzy12@gmail.com>
2025-01-21 23:21:13 +0700

    chore: show the daily income from orders

--- src/pages/Money.vue
+++ src/pages/Money.vue
@@ -1,7 +1,63 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, computed } from "vue";
+import { supabase } from "../supabase";
+import { formatCurrency } from "../utils/formatter/currency";
+
+const finance = ref([]);
+
+const fetchFinance = async () => {
+  try {
+    let { data: order_detail, error } = await supabase.from("order").select(`
+      id, 
+      paid,
+      total_price,
+      change
+      `);
+    if (error) throw error;
+    finance.value = order_detail;
+  } catch (error) {
+    console.log("error fetching order:", error);
+  }
+};
+
+const totalFinance = computed(() => {
+  const totalPaid = finance.value.reduce(
+    (sum, item) => sum + (item.paid || 0),
+    0
+  );
+  const totalChange = finance.value.reduce(
+    (sum, item) => sum + (item.change || 0),
+    0
+  );
+  return totalPaid - totalChange;
+});
+
+onMounted(fetchFinance);
+</script>
+
 <template>
-  <section class="main-section">
-    <h2>History Keuangan</h2>
-    <div class="card flex gap-5"></div>
-  </section>
+  <div class="p-6 flex flex-col gap-6">
+    <h1 style="font-size: 30px">Riwayat Keuangan</h1>
+    <section class="main-section">
+      <h1 class="text-4xl">
+        Total keuangan: {{ formatCurrency(totalFinance) }}
+      </h1>
+    </section>
+    <section class="main-section">
+      <div class="container flex flex-col gap-4">
+        <DataTable :value="finance" stripedRows tableStyle="min-width: 50rem">
+          <Column header="Total Masuk">
+            <template #body="slotProps">
+              {{ formatCurrency(slotProps.data.paid) }}
+            </template>
+          </Column>
+          <Column header="Kembalian">
+            <template #body="slotProps">
+              {{ formatCurrency(slotProps.data.change) }}
+            </template>
+          </Column>
+        </DataTable>
+      </div>
+    </section>
+  </div>
 </template>
