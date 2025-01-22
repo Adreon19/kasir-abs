b75b16f8acc96a269a83d4a5793ff3123d6f249d
Adreon <sachioramzy12@gmail.com>
2025-01-22 21:57:10 +0700

    chore: adding isLoading state, delete note from the table

--- src/pages/History.vue
+++ src/pages/History.vue
@@ -4,9 +4,11 @@ import { supabase } from "../supabase";
 import { formatCurrency } from "../utils/formatter/currency";
 
 const order = ref([]);
+const isLoading = ref(false);
 
 const fetchOrder = async () => {
   try {
+    isLoading.value = true;
     let { data: order_detail, error } = await supabase.from("order_detail")
       .select(`
       id, 
@@ -26,13 +28,14 @@ const fetchOrder = async () => {
           name
         )
       ),
-      quantity,
-      note
+      quantity
       `);
     if (error) throw error;
     order.value = order_detail;
   } catch (error) {
     console.log("error fetching order:", error);
+  } finally {
+    isLoading.value = false;
   }
 };
 
@@ -42,26 +45,30 @@ onMounted(fetchOrder);
 <template>
   <div class="p-6 flex flex-col gap-6">
     <h1 style="font-size: 30px">Riwayat Pesanan</h1>
-    <section class="main-section">
-      <DataTable :value="order" stripedRows tableStyle="min-width: 50rem">
-        <Column field="order_id.customer_name" header="Customer Name" />
-        <Column field="order_id.payment.name" header="Payment Method" />
-        <Column field="menu_detail_id.menu_id.name" header="Menu Name" />
-        <Column field="quantity" header="Quantity" />
-        <Column field="note" header="Note" />
-        <Column header="Total">
-          <template #body="slotProps">
-            {{ formatCurrency(slotProps.data.order_id.total_price) }}
-          </template>
-        </Column>
+    <div v-if="isLoading" class="flex justify-center">
+      <progressSpinner />
+    </div>
+    <div v-else>
+      <section class="main-section">
+        <DataTable :value="order" stripedRows tableStyle="min-width: 50rem">
+          <Column field="order_id.customer_name" header="Customer Name" />
+          <Column field="order_id.payment.name" header="Payment Method" />
+          <Column field="menu_detail_id.menu_id.name" header="Menu Name" />
+          <Column field="quantity" header="Quantity" />
+          <Column header="Total">
+            <template #body="slotProps">
+              {{ formatCurrency(slotProps.data.order_id.total_price) }}
+            </template>
+          </Column>
 
-        <!-- Custom rendering for Paid column -->
-        <Column header="Paid">
-          <template #body="slotProps">
-            {{ formatCurrency(slotProps.data.order_id.paid) }}
-          </template>
-        </Column>
-      </DataTable>
-    </section>
+          <!-- Custom rendering for Paid column -->
+          <Column header="Paid">
+            <template #body="slotProps">
+              {{ formatCurrency(slotProps.data.order_id.paid) }}
+            </template>
+          </Column>
+        </DataTable>
+      </section>
+    </div>
   </div>
 </template>
