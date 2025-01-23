302b3dd74090751062298860440a98fa316ef621
Adreon <sachioramzy12@gmail.com>
2025-01-23 15:34:00 +0700

    chore: outcome table

--- src/pages/Money.vue
+++ src/pages/Money.vue
@@ -2,9 +2,14 @@
 import { ref, onMounted, computed } from "vue";
 import { supabase } from "../supabase";
 import { formatCurrency } from "../utils/formatter/currency";
+import { DataTable, Toast, useToast } from "primevue";
 
+const toast = useToast();
 const isLoading = ref(false);
 const finance = ref([]);
+const outcomeData = ref([]);
+const outcomeDetail = ref("");
+const detail = ref("");
 
 const fetchFinance = async () => {
   try {
@@ -24,6 +29,53 @@ const fetchFinance = async () => {
   }
 };
 
+const fetchOutcome = async () => {
+  try {
+    isLoading.value = true;
+    let { data: outcome, error } = await supabase.from("outcome").select(`
+    pengeluaran,
+    catatan
+    `);
+    if (error) throw error;
+    outcomeData.value = outcome;
+  } catch (error) {
+    console.log("Error fetching outcome data:", error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+const insertOutcome = async () => {
+  try {
+    let { data: outcome, error } = await supabase.from("outcome").insert([
+      {
+        pengeluaran: outcomeDetail.value,
+        catatan: detail.value,
+      },
+    ]);
+    if (error) throw error;
+
+    toast.add({
+      severity: "success",
+      summary: "Success",
+      detail: "Pengeluaran berhasil dicatat!",
+      life: 3000,
+    });
+    outcomeDetail.value = "";
+    await fetchOutcome();
+    outcomeDetail.value = "";
+    detail.value = "";
+  } catch (error) {
+    console.log("error inserting outcome data:", error);
+    toast.add({
+      severity: "error",
+      summary: "Error",
+      detail: "Error mencatat pengeluaran!",
+      life: 3000,
+    });
+  }
+};
+
 const totalFinance = computed(() => {
   const totalPaid = finance.value.reduce(
     (sum, item) => sum + (item.paid || 0),
@@ -33,12 +85,17 @@ const totalFinance = computed(() => {
     (sum, item) => sum + (item.change || 0),
     0
   );
-  return totalPaid - totalChange;
+  const totalOutcome = outcomeData.value.reduce(
+    (sum, item) => sum + (item.pengeluaran || 0),
+    0
+  );
+  return totalPaid - (totalChange + totalOutcome);
 });
 
 const initializeData = async () => {
   try {
     await fetchFinance();
+    await fetchOutcome();
   } catch (error) {
     console.error("Error during initialization:", error.message);
   } finally {
@@ -55,7 +112,7 @@ onMounted(initializeData);
     <div v-if="isLoading" class="flex justify-center">
       <ProgressSpinner />
     </div>
-    <div v-else>
+    <div v-else class="flex flex-col gap-6">
       <section class="main-section">
         <h1 class="text-4xl">
           Total keuangan: {{ formatCurrency(totalFinance) }}
@@ -77,6 +134,56 @@ onMounted(initializeData);
           </DataTable>
         </div>
       </section>
+
+      <section class="main-section flex flex-col gap-4">
+        <div class="flex justify-between item-center gap-3">
+          <div class="flex flex-col gap-3">
+            <label for="outcome"> Pengeluaran </label>
+            <InputNumber
+              v-model="outcomeDetail"
+              id="outcome"
+              mode="currency"
+              currency="IDR"
+              locale="id-ID"
+              class="h-full text-l max-w-fit"
+            />
+          </div>
+          <div class="flex flex-col gap-2">
+            <label for="detail"> Detail </label>
+            <TextArea
+              v-model="detail"
+              row="5"
+              cols="30"
+              placeholder="Masukkan detail pengeluaranmu!"
+            />
+          </div>
+          <div class="flex justify-center items-center">
+            <Button
+              label="Save"
+              icon="fa fa-check"
+              iconPos="left"
+              @click="insertOutcome"
+              :loading="isLoading"
+              class="w-fit h-fit"
+            />
+          </div>
+        </div>
+        <div class="container flex flex-col gap-4">
+          <DataTable
+            :value="outcomeData"
+            stripedRows
+            tableStyle="min-width: 50rem"
+          >
+            <Column header="Pengeluaran">
+              <template #body="slotProps">
+                {{ formatCurrency(slotProps.data.pengeluaran) }}
+              </template>
+            </Column>
+            <Column field="catatan" header="Detail Pengeluaran" />
+          </DataTable>
+        </div>
+      </section>
     </div>
   </div>
+  <Toast />
 </template>
