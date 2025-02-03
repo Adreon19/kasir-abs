2aff3daf450f1fdaadd82e3dffc0129d0804c4f9
Adreon <sachioramzy12@gmail.com>
2025-02-03 19:43:02 +0700

    import: Variants

--- src/pages/Add.vue
+++ src/pages/Add.vue
@@ -4,12 +4,14 @@ import AddCategory from "../components/items/menu/AddCategory.vue";
 import ListCategory from "../components/items/menu/ListCategory.vue";
 import ListMenu from "../components/items/menu/ListMenu.vue";
 import AddPrice from "../components/items/menu/AddPrice.vue";
+import AddVariants from "../components/items/variants/AddVariants.vue";
 </script>
 <template>
   <div class="flex flex-col gap-4 max-w-screen-2xl mx-auto p-4">
     <Add />
     <AddPrice />
     <AddCategory />
+    <AddVariants />
     <ListMenu />
     <ListCategory />
   </div>
