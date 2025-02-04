85ccc70ce60e158dcff194a4971af36c3246f7f6
Adreon <sachioramzy12@gmail.com>
2025-02-04 11:05:06 +0700

    layout: deleting listCategory

--- src/pages/Add.vue
+++ src/pages/Add.vue
@@ -1,7 +1,6 @@
 <script setup>
 import Add from "../components/items/menu/AddMenu.vue";
 import AddCategory from "../components/items/menu/AddCategory.vue";
-import ListCategory from "../components/items/menu/ListCategory.vue";
 import ListMenu from "../components/items/menu/ListMenu.vue";
 import AddPrice from "../components/items/menu/AddPrice.vue";
 import AddVariants from "../components/items/variants/Variants.vue";
@@ -10,10 +9,9 @@ import AddVariants from "../components/items/variants/Variants.vue";
   <div class="flex flex-col gap-4 max-w-screen-2xl mx-auto p-4">
     <Add />
     <AddPrice />
+    <ListMenu />
     <AddCategory />
     <AddVariants />
-    <ListMenu />
-    <ListCategory />
   </div>
   <Toast />
 </template>
